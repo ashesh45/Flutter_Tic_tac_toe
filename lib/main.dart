78b9145f5ec714b1e1c55657ad3db52cf793dd84
Ashesh Maharjan <asheshmhrzn21@gmail.com>
2022-11-19 22:03:42 +0545

    tic tac toe

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'Home_page.dart';
+
+void main() => runApp(const MaterialApp(
+      title: "Tic Tac Toe",
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
